99eab94cd623dc3c63c7d86aacdc5074c3757d4e
camillawf <caamifigueiredo@gmail.com>
2023-08-31 20:37:05 -0300

    first

---
+++ src/main/java/br/edu/univille/projfab20232/Projfab20232Application.java
@@ -0,0 +1,13 @@
+package br.edu.univille.projfab20232;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projfab20232Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projfab20232Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/univille/projfab20232/Projfab20232ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.univille.projfab20232;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projfab20232ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
